704080b8d892090b90781c73ab21c6cec34de015
seyedjavadseyedi <sjsp.seyedi@gmail.com>
2020-04-26 21:39:58 +0430

    RTL configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { theme } from './settings/ThemeConfig'
+import { RTLConfig } from './settings/RTLConfig'
 
 function App() {
-  return <div>hello</div>
+  return (
+    <ThemeProvider theme={theme}>
+      <RTLConfig></RTLConfig>
+    </ThemeProvider>
+  )
 }
 
 export default App

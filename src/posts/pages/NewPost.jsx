983a7c39164d44ef74bc6da078749d77e71601b7
seyedjavadseyedi <sjsp.seyedi@gmail.com>
2020-04-29 22:12:44 +0430

    create custom hook for form

--- src/posts/pages/NewPost.jsx
+++ src/posts/pages/NewPost.jsx
@@ -1,61 +1,24 @@
-import React, { useCallback, useReducer } from 'react'
+import React from 'react'
 
 import Input from '../../shared/components/formElements/Input'
 import Button from '../../shared/components/formElements/Button'
 import { validatorRequire } from '../../shared/utils/validators'
-
-const initialState = {
-  inputs: {
-    title: {
-      value: '',
-      isValid: false,
-    },
-    description: {
-      value: '',
-      isValid: false,
-    },
-  },
-  isValid: false,
-}
-const fromReducer = (state, action) => {
-  switch (action.type) {
-    case 'INPUT_CHANGE':
-      let formIsValid = true
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: {
-            value: action.value,
-            isValid: action.isValid,
-          },
-        },
-        isValid: formIsValid,
-      }
-    default:
-      return state
-  }
-}
+import { useForm } from '../../shared/hooks/formHook'
 
 const NewPost = () => {
-  const [state, dispatch] = useReducer(fromReducer, initialState)
-
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: 'INPUT_CHANGE',
-      value: value,
-      isValid: isValid,
-      inputId: id,
-    })
-  }, [])
-
+  const [state, inputHandler] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+    },
+    false
+  )
   const postSubmitHandler = (e) => {
     e.preventDefault()
     console.log(state.inputs)

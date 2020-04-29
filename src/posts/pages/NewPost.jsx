91427343833661f0c91e328a7d3cf8df6b456074
seyedjavadseyedi <sjsp.seyedi@gmail.com>
2020-04-29 21:47:49 +0430

    create input component

--- src/posts/pages/NewPost.jsx
+++ src/posts/pages/NewPost.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import React, { useCallback, useReducer } from 'react'
+
 import Input from '../../shared/components/formElements/Input'
+import Button from '../../shared/components/formElements/Button'
+import { validatorRequire } from '../../shared/utils/validators'
+
+const initialState = {
+  inputs: {
+    title: {
+      value: '',
+      isValid: false,
+    },
+    description: {
+      value: '',
+      isValid: false,
+    },
+  },
+  isValid: false,
+}
+const fromReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: {
+            value: action.value,
+            isValid: action.isValid,
+          },
+        },
+        isValid: formIsValid,
+      }
+    default:
+      return state
+  }
+}
 
 const NewPost = () => {
+  const [state, dispatch] = useReducer(fromReducer, initialState)
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    })
+  }, [])
+
+  const postSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(state.inputs)
+  }
+
   return (
     <div>
-      <form>
-        <Input element='input' type='text' placeholder='عنوان پست...' />
+      <form onSubmit={postSubmitHandler}>
+        <Input
+          id='title'
+          element='input'
+          type='text'
+          placeholder='عنوان پست...'
+          errorText='عنوان را وارد کنید'
+          validators={[validatorRequire()]}
+          onInput={inputHandler}
+        />
+        <Input
+          id='description'
+          element='textarea'
+          placeholder='توضیحات پست...'
+          errorText='توضیحات را اضافه کنید'
+          validators={[validatorRequire()]}
+          onInput={inputHandler}
+        />
+        <Button type='submit' disabled={!state.isValid}>
+          اضافه کردن پست
+        </Button>
       </form>
     </div>
   )
--- src/shared/components/formElements/Input.jsx
+++ src/shared/components/formElements/Input.jsx
@@ -1,13 +1,85 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react'
+import { validate } from '../../utils/validators'
 
-const Input = ({ element, id, type, placeholder, rows }) => {
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.inputValue,
+        isValid: validate(action.inputValue, action.validators),
+      }
+    case 'BLUR':
+      return {
+        ...state,
+        isBlured: true,
+      }
+    default:
+      return state
+  }
+}
+
+const initialState = {
+  value: '',
+  isValid: false,
+  isBlured: false,
+}
+
+const Input = ({
+  element,
+  id,
+  onInput,
+  type,
+  placeholder,
+  rows,
+  errorText,
+  validators,
+}) => {
+  const [state, dispatch] = useReducer(inputReducer, initialState)
+
+  const { value, isValid } = state
+
+  useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput])
+
+  const changeHandler = (e) => {
+    dispatch({
+      type: 'CHANGE',
+      inputValue: e.target.value,
+      validators: validators,
+    })
+  }
+
+  const blurHandler = () => {
+    dispatch({
+      type: 'BLUR',
+    })
+  }
+
   return (
     <div>
       {element === 'input' ? (
-        <input id={id} type={type} placeholder={placeholder} />
+        <input
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          onChange={changeHandler}
+          value={state.value}
+          onBlur={blurHandler}
+        />
       ) : (
-        <textarea id={id} rows={rows || 3} />
+        <textarea
+          id={id}
+          rows={rows || 3}
+          placeholder={placeholder}
+          onChange={changeHandler}
+          value={state.value}
+          onBlur={blurHandler}
+        />
       )}
+
+      {!state.isValid && state.isBlured && <p>{errorText}</p>}
     </div>
   )
 }
